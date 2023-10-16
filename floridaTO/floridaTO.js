a9527721b49e27991163ec6c610f64be08b3a076
Jude Anthony Cadano <Coder3lite@gmail.com>
2023-10-16 18:50:32 +0800

    Update floridaTO.js

--- floridaTO/floridaTO.js
+++ floridaTO/floridaTO.js
@@ -407,7 +407,7 @@ function calcTmc(){
 	}
 
 	document.getElementById("tprice").value = total;
-	document.getElementById("ftprice").value = fttotal;
+	document.getElementById("ftprice").value = fttotal * day;
 
 }
 
